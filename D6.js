2fb0ba158fb7608424c6a80716061d90bc69adbe
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2022-12-12 17:02:05 +0100

    finished 1.1

---
+++ D6.js
@@ -0,0 +1,166 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato..
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const area = (l1, l2) => l1 * l2
+
+console.log(area(3, 3))
+
+
+
+
+/* ESERCIZIO 2
+ Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
+ la loro somma moltiplicata per tre.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+const crazySum = (i1, i2) => {
+    if (i1 !== i2) {
+        return i1 + i2
+    }
+    else {
+        return (i1 + i2) * 3
+    }
+}
+
+console.log(crazySum(10, 10))
+
+
+
+/* ESERCIZIO 3
+ Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
+ Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const crazyDiff = (n1) => {
+    if (n1 > 19) {
+        return (Math.abs(n1 - 19)) * 3
+
+    }
+    else {
+        return Math.abs(n1 - 19)
+    }
+}
+console.log(crazyDiff(25))
+
+
+
+/* ESERCIZIO 4
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ se n è uguale a 400.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+const boundary = (n) => {
+    if (n === 400 || (n >= 20 && n <= 100)) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+console.log(boundary(19))
+
+
+
+/* ESERCIZIO 5
+ Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
+ ritornare la stringa originale senza alterarla.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const epify = (stringa) => {
+    if (typeof (stringa) === "string") {
+        if (stringa.split(" ")[0] === "EPICODE") {
+            return stringa
+
+        }
+        else {
+            return "EPICODE".concat(" ", stringa)
+        }
+    }
+    else {
+        return "please insert a valid string"
+    }
+}
+console.log(epify("ADAD ASDAS ADS my name is"))
+
+
+
+
+
+/* ESERCIZIO 6
+ Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
+ di 3 o di 7. (Suggerimento: usa l'operatore modulo)
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+const check3and7 = (nPositivo) => {
+    if (nPositivo >= 0) {
+        if (nPositivo % 3 === 0 || nPositivo % 7 === 0) {
+            return "è multiplo di 3 o 7"
+        }
+        else {
+            return "non è multiplo di 3 o 7"
+        }
+    }
+    else {
+        return "please insert a positive number"
+    }
+}
+
+console.log(check3and7(21))
+/* ESERCIZIO 7
+ Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+const reverseString = (invert) => {
+    let invertED = ""
+    for (let i = invert.length - 1; i >= 0; i--) {
+        invertED = invertED + invert[i]
+        return invert
+    }
+}
+
+
+
+
+
+/* ESERCIZIO 8
+ Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
+ La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 9
+ Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
+ della stringa originale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 10
+ Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
